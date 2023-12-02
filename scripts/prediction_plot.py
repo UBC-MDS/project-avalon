0dbb217ab0b43604835cc299f6bb5b86028397d7
Po-Hsun Chen <benjamin990126@hotmail.com>
2023-12-02 14:30:51 -0800

    turn prediction plot into script

---
+++ scripts/prediction_plot.py
@@ -0,0 +1,54 @@
+import click
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+@click.command()
+@click.argument('prediction_df')
+@click.argument('results_folder')
+
+def main(prediction_df, results_folder):
+
+    data = pd.read_csv(prediction_df, index_col=[0])
+    data.index = pd.to_datetime(data.index)
+    print(data)
+
+    # ARIMA Plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(data.index, data['Observations'], label='Original Values', color='black')
+    plt.plot(data.index, data['ARIMA_Forecast'], label='ARIMA Forecasts', color='orange')
+    plt.legend()
+    plt.title('ARIMA Predictions to Original Value')
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.savefig(os.path.join(results_folder, "figures", "arima_prediction_plot.png"))
+
+    # ES Plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(data.index, data['Observations'], label='Original Values', color='black')
+    plt.plot(data.index, data['ES_Forecast'], label='ES Forecasts', color='blue')
+    plt.legend()
+    plt.title('ES Predictions to Original Value')
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.savefig(os.path.join(results_folder, "figures", "es_prediction_plot.png"))
+
+    # SMA Plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(data.index, data['Observations'], label='Original Values', color='black')
+    plt.plot(data.index, data['SMA_Forecast'], label='SMA Forecasts', color='green')
+    plt.legend()
+    plt.title('SMA Predictions to Original Value')
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.savefig(os.path.join(results_folder, "figures", "sma_prediction_plot.png"))
+
+
+if __name__ == '__main__':
+    main()
