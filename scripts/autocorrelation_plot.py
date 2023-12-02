8480e63e7f763a16287b3a2053ff66f4e66cf2bd
Po-Hsun Chen <benjamin990126@hotmail.com>
2023-12-02 14:40:39 -0800

    add autocorrelation script and png

---
+++ scripts/autocorrelation_plot.py
@@ -0,0 +1,36 @@
+import click
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pandas.plotting import autocorrelation_plot
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+
+@click.command()
+@click.argument('preprocessed_data')
+@click.argument('results_folder')
+
+def main(preprocessed_data, results_folder):
+
+    data = pd.read_csv(preprocessed_data)
+    data = data[['YEAR-MONTH','Observations']]
+    data.set_index('YEAR-MONTH', inplace=True)
+    data.index = pd.to_datetime(data.index)
+
+    # Autocorrelation Plot
+    autocorrelation_plot(data.Observations)
+    plt.savefig(os.path.join(results_folder, "figures", "autocorrelation_plot.png"))
+    plt.clf()
+
+    # Autocorrelation Plot with diff=1
+    data_diff = data.diff().dropna() 
+    autocorrelation_plot(data_diff.Observations)
+    plt.savefig(os.path.join(results_folder, "figures", "autocorrelation_with_diff_plot.png"))
+
+
+if __name__ == '__main__':
+    main()
